d4bf2913b5b652d65f142fda11295035fccafd78
Jonatas Rafael Carneiro Ribeiro <jonatasrafael@jonatasrafael.com>
2020-06-14 12:25:22 -0300

    filters applied to the map

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -1,30 +1,76 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Feather as Icon, FontAwesome } from '@expo/vector-icons';
-import { View, Text, StyleSheet, TouchableOpacity, Image, SafeAreaView } from 'react-native'
-import { useNavigation } from '@react-navigation/native';
+import { View, Text, StyleSheet, TouchableOpacity, Image, SafeAreaView, Linking } from 'react-native'
+import { useNavigation, useRoute } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
+import api from '../../services/api';
+import * as MailComposer from 'expo-mail-composer';
+
+interface Params {
+  point_id: number;
+}
+
+interface Data {
+  point: {
+    image:string;
+    name:string;
+    email:string;
+    whatsapp:string;
+    city:string;
+    uf:string;
+  };
+  items: {
+    title: string;
+  }[];
+}
 
 const Detail = () => {
+  const [data, setData] = useState<Data>({} as Data);
+
   const navigation = useNavigation();
+  const route = useRoute();
+
+  const routeParams = route.params as Params;
+
+  useEffect(() => {
+    api.get(`points/${routeParams.point_id}`).then(response => {
+      setData(response.data);
+    });
+  }, [])
 
   function handleNavigateBack() {
     navigation.goBack();
   }
 
+  function handleComposeMail() {
+    MailComposer.composeAsync({
+      subject: 'Interesse na coleta de resíduos',
+      recipients: [data.point.email],
+    });
+  }
+
+  function handleWhatsapp() {
+    Linking.openURL(`whatsapp://send?phone=${data.point.whatsapp}&text=Tenho interesse sobre coleta de resíduos`);
+  }
+
+  if(!data.point) {
+    return null;
+  }
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <View>
+      <View style={styles.container}>
         <TouchableOpacity onPress={handleNavigateBack}>
           <Icon name="arrow-left" size={20} color="#34cb79" />
         </TouchableOpacity>
 
-        <Image style={styles.pointImage} source={{ uri: 'http://images.unsplash.com/photo-1542838132-92c53300491e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60'}} />
+        <Image style={styles.pointImage} source={{ uri: data.point.image}} />
 
         <Text style={styles.pointImage}>
-          Mercado Brasão
+          {data.point.name}
         </Text>
         <Text style={styles.pointItems}>
-          Lampadas, Pleo de cozinha
+          {data.items.map(item => item.title).join(', ')}
         </Text>
 
         <View style={styles.address}>
@@ -32,20 +78,20 @@ const Detail = () => {
             Endereço
           </Text>
           <Text style={styles.addressContent}>
-            Rio do sul
+            {data.point.city}, {data.point.uf}
           </Text>
         </View>
       </View>
 
       <View style={styles.footer}>
-        <RectButton style={styles.button} onPress={()=>{}}>
+        <RectButton style={styles.button} onPress={handleWhatsapp}>
           <FontAwesome name="whatsapp" size={20} color="#FFF" />
           <Text style={styles.buttonText}>
             Whatsapp
           </Text>
         </RectButton>
 
-        <RectButton style={styles.button} onPress={() => { }}>
+        <RectButton style={styles.button} onPress={handleComposeMail}>
           <Icon name="mail" size={20} color="#FFF" />
           <Text style={styles.buttonText}>
             E-mail
@@ -66,7 +112,7 @@ const styles = StyleSheet.create({
   pointImage: {
     width: "100%",
     height: 120,
-    resizeMode: "cover",
+    // resizeMode: "cover",
     borderRadius: 10,
     marginTop: 32,
   },
@@ -107,7 +153,6 @@ const styles = StyleSheet.create({
     borderTopWidth: StyleSheet.hairlineWidth,
     borderColor: "#999",
     paddingVertical: 20,
-    paddingBottom: 0,
     paddingHorizontal: 32,
     flexDirection: "row",
     justifyContent: "space-between",
